157664947ec36555a52c8a4a84789cc1344de79c
anugagganapally <116759592+anugagganapally@users.noreply.github.com>
2025-12-02 13:58:07 -0500

    Add Day 1 basic Python examples

---
+++ day1_basics.py
@@ -0,0 +1,16 @@
+# Print a message
+print("Hello, AI World!")
+
+# Simple arithmetic
+a = 5
+b = 3
+print("Sum:", a + b)
+
+# If-else example
+age = int(input("Enter your age: "))
+if age < 13:
+    print("You are a Child")
+elif 13 <= age <= 17:
+    print("You are a Teenager")
+else:
+    print("You are an Adult")
